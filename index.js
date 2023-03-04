ff18c599bc234e9f26f2c275dff764994c4b9ed9
stasispuha <stasispuha@gmail.com>
2023-03-04 16:51:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client } = require("discord.js");
+const config = require("./config.json");
+const client = new Client();
+
+
+client.on('ready', () => {
+    console.log("READY!");
+});
+
+client.on('message', msg => { // Message function
+    if (msg.author.bot) return; // Ignore all bots
+    if (!msg.content.startsWith('!'))  return; // It always has to starts with the prefix which is '!'
+
+
+    if (msg.content.startsWith('!' + "ping")) { // When a player does '!ping'
+        msg.reply("Pong!") // The bot will say @Author, Pong!
+    }
+});
+
+client.login(config["Bot_Info"].token);
