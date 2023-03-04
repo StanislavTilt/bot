99f28cb9c48aa4a22876b15a65397fed306d74aa
stasispuha <stasispuha@gmail.com>
2023-03-04 16:52:40 +0200

    M  index.js

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ client.on('ready', () => {
 client.on('message', msg => { // Message function
     if (msg.author.bot) return; // Ignore all bots
     if (!msg.content.startsWith('!'))  return; // It always has to starts with the prefix which is '!'
-
-
+    
     if (msg.content.startsWith('!' + "ping")) { // When a player does '!ping'
         msg.reply("Pong!") // The bot will say @Author, Pong!
     }
